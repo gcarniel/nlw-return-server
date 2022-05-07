c974aa1ee108ef9d53d77e658c65847d08841953
gcarniel <gcarniel@outlook.com>
2022-05-07 10:17:37 -0300

    adicionando port

--- src/server.ts
+++ src/server.ts
@@ -3,12 +3,11 @@ import { routes } from "./routes";
 import cors from "cors";
 
 const app = express();
-const door = 3333;
 
 app.use(cors());
 app.use(express.json());
 app.use(routes);
 
-app.listen(door, () => {
-  console.log(`Running in door ${door}`);
+app.listen(process.env.PORT || 3333, () => {
+  console.log(`Running in running`);
 });
